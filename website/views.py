12f354adce15b6d858fcc316f2bae186cf11354e
Rayhan B <64879192+rayb26@users.noreply.github.com>
2022-01-16 18:49:25 -0700

    Add files via upload

--- website/views.py
+++ website/views.py
@@ -9,7 +9,7 @@ views = Blueprint('views', __name__)
 @views.route('/view-data', methods=['GET'])
 def view_data():
 
-    return render_template('view-data.html', terms=['term1', 'term2', 'term3'])
+    return render_template('view-data.html', terms=Database.read_data())
 
 
 @views.route('/add-term', methods=['GET', 'POST'])
@@ -18,8 +18,8 @@ def add_term():
     data = request.form
 
     if request.method == 'POST' and len(data.get('term')) > 0 and len(data.get('definition')) > 0:
-        #Database.add_term_def(data.get('term'), data.get('definition'))
+        Database.add_term_def(data.get('term'), data.get('definition'))
         flash("Term Added", category='success')
         return redirect(url_for('views.add_term'))
 
-    return render_template('add-term.html', terms=['term1', 'term2', 'term3'])
+    return render_template('add-term.html', terms=Database.read_data())
